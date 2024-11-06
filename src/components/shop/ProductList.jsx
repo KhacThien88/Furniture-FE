e4d089d63b9170cc56771237dc6bff900c0a8cb6
ThuanPham777 <phamchithuan777@gmail.com>
2024-11-06 16:30:17 +0700

    update productList

--- src/components/shop/ProductList.jsx
+++ src/components/shop/ProductList.jsx
@@ -1,29 +1,28 @@
-import React, { useEffect, useState } from "react";
-import ProductCard from "../../components/ProductCard";
-import SortDropdown from "./SortDropdown";
+import React, { useEffect, useState } from 'react';
+import ProductCard from '../../components/ProductCard';
+import SortDropdown from './SortDropdown';
 
 const ProductList = () => {
   const [data, setData] = useState([]);
   const [initialData, setInitialData] = useState([]); // Store initial data
 
   useEffect(() => {
-    fetch("https://furniture-be-od3w.onrender.com/api/products")
+    fetch('https://furniture-be-od3w.onrender.com/api/products')
       .then((res) => {
-        //console.log('Response status:', res.status); // Kiểm tra status code
         return res.json();
       })
       .then((data) => {
         setData(data);
-        console.log("data: ", data);
+        console.log('data: ', data);
         setInitialData(data);
       })
-      .catch((err) => console.error("Fetch error:", err));
+      .catch((err) => console.error('Fetch error:', err));
   }, []);
 
   if (!data) {
     return (
-      <div className="flex items-center justify-center h-screen">
-        <p className="text-lg font-semibold text-gray-600 animate-pulse">
+      <div className='flex items-center justify-center h-screen'>
+        <p className='text-lg font-semibold text-gray-600 animate-pulse'>
           Loading product List...
         </p>
       </div>
@@ -32,8 +31,11 @@ const ProductList = () => {
 
   return (
     <>
-      <SortDropdown initialData={initialData} sortedData={setData} />
-      <div className="w-full grid lg:grid-cols-3 sm:grid-cols-2 grid-cols-1 py-3 gap-3">
+      <SortDropdown
+        initialData={initialData}
+        sortedData={setData}
+      />
+      <div className='w-full grid lg:grid-cols-3 sm:grid-cols-2 grid-cols-1 py-3 gap-3'>
         {data.length > 0 &&
           data.map((product) => (
             <div key={product._id}>
